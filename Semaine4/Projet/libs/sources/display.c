5696955db700ab3c6b8ad9ef2d6bd01b03639142
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2024-02-21 14:26:46 +0100

    init

--- Semaine4/Projet/libs/sources/display.c
+++ Semaine4/Projet/libs/sources/display.c
@@ -1,30 +1,30 @@
 #include "display.h"
 
-void displayWord(mot_data_t* word, FILE *filedes) {
+void displayWord(mot_data_t *word, FILE *filedes) {
   if (word == NULL) {
   } else {
-    char* link = word->lemot;   
-    emplacement_t* list = word->tete_liste;
+    char *link = word->lemot;
+    emplacement_t *list = word->tete_liste;
     if (link != NULL) {
-      fprintf(filedes,"%s",link);
+      fprintf(filedes, "%s", link);
       fflush(stdout);
     }
-    while(list != NULL) {
-      fprintf(filedes," (%i,%i)",list->line,list->colonne);
+    while (list != NULL) {
+      fprintf(filedes, " (%i,%i)\n", list->line, list->colonne);
       list = list->next;
     }
   }
 }
 
-void displayNodes(dico *d, FILE* f) {
+void displayNodes(dico *d, FILE *f) {
   if (d) {
     displayNodes(d->fg, f);
-    displayWord(&(d->mot->data),f);
+    displayWord(&(d->mot->data), f);
     displayNodes(d->fd, f);
   }
 }
 
-void displayDico(dico* dictionary) {
+void displayDico(dico *dictionary) {
   FILE *f = NULL;
   f = fopen(DICORES, "w+");
   if (f == NULL) {
@@ -37,7 +37,7 @@ void displayDico(dico* dictionary) {
     printf("displayDico : NULL\n");
   } else {
     fprintf(f, "Contenu dictionnaire pour %s : \n", TEXTE);
-    dico* tempDico = (dico*) malloc(sizeof(dico));
+    dico *tempDico = (dico *)malloc(sizeof(dico));
     tempDico = dictionary;
     displayNodes(tempDico, f);
     fflush(f);
