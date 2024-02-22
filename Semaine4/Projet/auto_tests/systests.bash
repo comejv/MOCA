26bb938fce712e11a6237b582df77e96c74da37a
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2024-02-22 13:14:54 +0100

    ça marche (?)

--- Semaine4/Projet/auto_tests/systests.bash
+++ Semaine4/Projet/auto_tests/systests.bash
@@ -1,26 +1,58 @@
 #!/bin/bash
 
-# File to read from and write to
-input_file=$1
-output_file=$2
+res_dir=auto_tests/res
+mkdir -p $res_dir
 
-# Process the file
-awk '
-/^Index/ {
-    if (count >  0) {
-        print word, count > "'"$output_file"'"
-    }
-    count=0
+calculate_word_frequency() {
+    src_file="$1"
+    dest_file="$2"
+
+    # Check if source file exists
+    if [ ! -f "$src_file" ]; then
+        echo "Source file does not exist."
+        exit  1
+    fi
+
+    tr -d '[:punct:]' < $src_file | tr ' ' '\n' | sort | uniq -c | sort -k2,2 | awk '{print $2, $1}' > $dest_file
 }
+
+parse_output ()
 {
-    count++
-    if (/^Index/) {
-        word=$4
-    }
+    # input_file="$1"
+    # output_file="$2"
+    awk '
+    /^Resultat/ { next } # Ignore lines starting with "Resultat"
+    /^---/ { exit } # Stop processing when a line starts with "---"
+    /^Index/ {
+         if (count >  0) {
+             print word, count
+         }
+         count=0
+     }
+     {
+         count++
+         if (/^Index/) {
+             word=$4
+         }
+     }
+     END {
+         if (count >  0) {
+             print word, count
+         }
+     }
+    ' - # < ./dico $input_file # | sort -k1 -o $output_file
+    return 
 }
-END {
-    if (count >  0) {
-        print word, count >> "'"$output_file"'"
-    }
-}
-' "$input_file"
+
+for txt in ./exemples/*.txt; do 
+    exp="exemples/expected/$(basename $txt)"
+    if [[ ! -f $exp ]]; then
+        calculate_word_frequency $txt $exp
+    fi
+    out=$res_dir/res_$(basename $txt)
+    ./dico $txt | parse_output | sort -k1 -o $out
+    diff -q $out $exp
+    if [[ $? -ne 0 ]]; then
+        echo "Test $txt failed"
+    fi
+done
